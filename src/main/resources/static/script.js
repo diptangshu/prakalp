7de7ead44b75a6c8e15110a13e7841ed633d1dd9
diptangshu <cdiptangshu@gmail.com>
2015-05-01 05:53:32 +0530

    Initial commit

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,27 @@
+$(function () {
+    $('.navbar-toggle').click(function () {
+        $('.navbar-nav').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+        $('#search').removeClass('in').addClass('collapse').slideUp(200);
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').toggleClass('slide-in');
+        
+    });
+   
+   // Remove menu for searching
+   $('#search-trigger').click(function () {
+        $('.navbar-nav').removeClass('slide-in');
+        $('.side-body').removeClass('body-slide-in');
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').removeClass('slide-in');
+
+    });
+    
+    $('#project_btn').on('click', function () {
+        $('#main').toggleClass('shift-left shift-right');
+        $('#menu_projects').toggleClass('in out');
+    });
+
+});
